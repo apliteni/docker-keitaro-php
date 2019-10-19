cdb9998d25ec3f118c1ed3fe7fb3198f64135544
Artur Sabirov <artur.sabirov@apliteni.com>
2019-10-19 07:30:14 +0200

    several images with composer

--- update.sh
+++ update.sh
@@ -8,8 +8,16 @@ sedStr="
 sed -E "$sedStr" $1
 }
 
+# base images
 versions=(56 70 71 72 73)
 for version in ${versions[*]}; do
   mkdir $version
   render Dockerfile.template > $version/Dockerfile
+done
+
+# images with composer
+versions=(56 70 71)
+for version in ${versions[*]}; do
+  mkdir ${version}-composer
+  render Dockerfile.composer.template > ${version}-composer/Dockerfile
 done
