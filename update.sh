bad62f7c6f50073765f2df75c72da88624abf951
Artur Sabirov <artur.sabirov@apliteni.com>
2019-10-19 10:06:23 +0200

    use latest alpine available

--- update.sh
+++ update.sh
@@ -3,21 +3,21 @@
 render() {
 sedStr="
   s!%%PHP_VERSION%%!$version!g;
-  s!%%ALPINE_VERSION%%!$alpine_version!g;
+  s!%%REDIS_VERSION%%!$redis_version!g;
 "
 
 sed -E "$sedStr" $1
 }
 
 # base images
-alpine_version=3.7
+redis_version="-2.2.8"
 versions=(5.6 7.0)
 for version in ${versions[*]}; do
   mkdir $version
   render Dockerfile.template > $version/Dockerfile
 done
 
-alpine_version=3.8
+redis_version=""
 versions=(7.1 7.2 7.3)
 for version in ${versions[*]}; do
   mkdir $version
