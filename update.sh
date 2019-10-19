57743b3b51cdb964e38b0602bd1c6b24a6503679
Artur Sabirov <artur.sabirov@apliteni.com>
2019-10-19 16:22:51 +0200

    fixes

--- update.sh
+++ update.sh
@@ -4,21 +4,32 @@ render() {
 sedStr="
   s!%%PHP_VERSION%%!$version!g;
   s!%%REDIS_VERSION%%!$redis_version!g;
+  s!%%ALPINE_VERSION%%!$alpine_version!g;
 "
 
 sed -E "$sedStr" $1
 }
 
-# base images
 redis_version="-2.2.8"
-versions=(5.6 7.0)
+alpine_version=""
+versions=(5.6)
 for version in ${versions[*]}; do
   mkdir $version
   render Dockerfile.template > $version/Dockerfile
 done
 
 redis_version=""
-versions=(7.1 7.2 7.3 7.4-rc)
+alpine_version="3.4"
+versions=(7.0)
+for version in ${versions[*]}; do
+  mkdir $version
+  render Dockerfile.template > $version/Dockerfile
+done
+
+
+redis_version=""
+alpine_version=""
+versions=(7.1 7.2 7.3)
 for version in ${versions[*]}; do
   mkdir $version
   render Dockerfile.template > $version/Dockerfile
