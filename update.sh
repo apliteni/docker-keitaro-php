ab537b6b621213b893d0d8a052f688923f56a104
Artur Sabirov <artur.sabirov@apliteni.com>
2019-10-24 22:41:36 +0200

    separate images to cli/fpm

--- update.sh
+++ update.sh
@@ -3,6 +3,7 @@
 render() {
 sedStr="
   s!%%PHP_VERSION%%!$version!g;
+  s!%%PHP_VARIANT%%!$variant!g;
   s!%%REDIS_VERSION%%!$redis_version!g;
   s!%%ALPINE_VERSION%%!$alpine_version!g;
 "
@@ -10,27 +11,30 @@ sedStr="
 sed -E "$sedStr" $1
 }
 
-redis_version="-2.2.8"
-alpine_version=""
-versions=(5.6)
-for version in ${versions[*]}; do
-  mkdir $version
-  render Dockerfile.template > $version/Dockerfile
-done
+variants=(fpm cli)
+for variant in ${variants[*]}; do
 
-redis_version=""
-alpine_version="3.4"
-versions=(7.0)
-for version in ${versions[*]}; do
-  mkdir $version
-  render Dockerfile.template > $version/Dockerfile
-done
+  redis_version="-2.2.8"
+  alpine_version=""
+  versions=(5.6)
+  for version in ${versions[*]}; do
+    mkdir $version-$variant
+    render Dockerfile.template > $version-$variant/Dockerfile
+  done
 
+  redis_version=""
+  alpine_version="3.4"
+  versions=(7.0)
+  for version in ${versions[*]}; do
+    mkdir $version-$variant
+    render Dockerfile.template > $version-$variant/Dockerfile
+  done
 
-redis_version=""
-alpine_version=""
-versions=(7.1 7.2 7.3)
-for version in ${versions[*]}; do
-  mkdir $version
-  render Dockerfile.template > $version/Dockerfile
+  redis_version=""
+  alpine_version=""
+  versions=(7.1 7.2 7.3)
+  for version in ${versions[*]}; do
+    mkdir $version-$variant
+    render Dockerfile.template > $version-$variant/Dockerfile
+  done
 done
